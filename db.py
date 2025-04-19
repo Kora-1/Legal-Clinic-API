cf409e4b95e6f4ce61f6796568f777e6f429e673
Kora-1 <koralalkora@gmail.com>
2025-04-19 21:55:16 +0530

    API Added

---
+++ db.py
@@ -0,0 +1,26 @@
+# from pymongo import MongoClient
+#
+# MONGO_URI = "mongodb+srv://accforapplinmongodb:tvX6lHov82OI0NTG@cluster0.mongodb.net/?retryWrites=true&w=majority"
+#
+# client = MongoClient(MONGO_URI)
+# db = client.simple_auth_db
+# users_collection = db.users
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://accforapplinmongodb:tvX6lHov82OI0NTG@datainfo.nqxv2sl.mongodb.net/?retryWrites=true&w=majority&appName=DataInfo"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.simple_auth_db
+users_collection = db.users
---
+++ main.py
@@ -0,0 +1,30 @@
+# from fastapi import FastAPI
+# from auth.routes import router as auth_router
+#
+# app = FastAPI()
+#
+# app.include_router(auth_router, prefix="/auth")
+#
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from auth.routes import router as auth_router  # Assuming auth.routes contains your router
+
+app = FastAPI()
+
+# CORS middleware setup
+origins = [
+    "*"
+]
+
+# Add CORSMiddleware to the app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific domains to access
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Include the router for authentication
+app.include_router(auth_router, prefix="/auth")
